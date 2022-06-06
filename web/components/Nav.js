8b552dcff188f2a218559476932b6229ee9f6e26
Eliott Moreno <eliott_mor@hotmail.com>
2022-06-06 00:13:39 -0600

    added layout and nav

---
+++ web/components/Nav.js
@@ -0,0 +1,11 @@
+import { styled } from '@stitches/react'
+
+const Nav = () => {
+  return <Title>TENNIS LADDER</Title>
+}
+
+const Title = styled('h3', {
+  marginLeft: '3rem'
+})
+
+export default Nav
